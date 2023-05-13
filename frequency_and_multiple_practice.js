fd8b9cec77ff157c5b93a6eceddbf7f71fff226e
Cuz-IAm <yameame320@gmail.com>
2023-05-13 23:04:39 +0900

    practice files reuploaded

---
+++ frequency_and_multiple_practice.js
@@ -0,0 +1,46 @@
+/*
+<문제>
+1.Implement a function called, 'areThereDuplicates'
+2.this function accepts 'variable number of arguments'
+3.this function checks whether there are any duplicates among the arguments passed in
+4.it can use frequency counter pattern or multiple pointers pattern.
+
+<제한사항>
+time complexity<=O(N), O(nlogn)일 경우 bonus
+space complexity<=O(N), O(1)일 경우 bonus
+
+<예시>
+areThereDuplicates(1, 2, 3) // false
+areThereDuplicates(1, 2, 2) // true 
+areThereDuplicates('a', 'b', 'c', 'a') // true 
+
+<input,output>
+의문... 숫자와 문자가 섞이는 경우는 없다고 가정하는 건가. 그럼 이진탐색 가능할듯 하다.
+input: 숫자로만 or 문자로만 되어있음. 인자는 여러개가 될 수 있음. 빈값 가능. 각 인자의 길이는 1.
+문자의 경우 영어 소문자만으로. 숫자의 경우 자연수로만 이루어졌다고 가정.
+output: boolean true or false
+
+<풀이>
+arguments 객체에 직접 접근하기.
+카운터에 넣고 만약에 카운터가 2이상이면 무조건 return false
+go
+글
+*/
+function sameThereDuplicates(...args) {
+  // 카운터 생성
+  const argsCounter = {};
+  // 카운터에 값 넣음
+  for (const value of args) {
+    argsCounter[value] = (argsCounter[value] || 0) + 1;
+  }
+  for (const key in argsCounter) {
+    if (Object.prototype.hasOwnProperty.call(argsCounter, key)) {
+      if (argsCounter[key] > 1) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+console.log(sameThereDuplicates(2, 4, 9, 9, 9));
