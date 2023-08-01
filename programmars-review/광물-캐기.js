fb9b4902f68b9b6081c333cd00a30421467e7506
Cuz-IAm <yameame320@gmail.com>
2023-08-01 10:46:01 +0900

    광물 캐기

--- programmars-review/광물-캐기.js
+++ programmars-review/광물-캐기.js
@@ -1,61 +1,78 @@
+/**
+중복조합으로 풀 수도 잇을 것 같다.
+3^5=243 정도 밖에 안된다.
+*/
+
+function findAllCombination(arr, length) {
+  // base case
+  if (length === 1) return arr.map(ele => [ele]);
+  // recursive case
+  const combinations = [];
+  for (let i = 0; i < arr.length; i++) {
+    const rest = findAllCombination(arr, length - 1);
+    for (let j = 0; j < rest.length; j++) {
+      combinations.push([arr[i], ...rest[j]]);
+    }
+  }
+  return combinations;
+}
+
 function solution(picks, minerals) {
-  const totalFatigue = 0;
-  const fatigues = {
-    diamond: {
+  let totalFatigue = 0;
+  const fatigues = [
+    {
       diamond: 1,
       iron: 1,
       stone: 1,
     },
-    iron: {
+    {
       diamond: 5,
       iron: 1,
       stone: 1,
     },
-    stone: {
+    {
       diamond: 25,
       iron: 5,
       stone: 1,
     },
-  };
-  while (minerals.length > 0 && picks.some(tool => tool !== 0)) {
-    // minerals에서 5개씩 읽어오기
+  ];
+  minerals = minerals.slice(
+    0,
+    picks.reduce((acc, cur) => acc + 5 * cur, 0)
+  );
+  let fiveMineralsArr = [];
+  while (minerals.length > 0) {
     const fiveMinerals = minerals.splice(0, 5);
     const mineralsCount = {};
-    // counter만들기
     fiveMinerals.forEach(name => {
       mineralsCount[name] = (mineralsCount[name] || 0) + 1;
     });
-    console.log(mineralsCount);
-    // 사용할 도구 고르기
-    let toolToUse = '';
-    if (mineralsCount.diamond && picks[0] > 0) {
-      toolToUse = 'diamond';
-      picks[0]--;
-    } else if (mineralsCount.iron && picks[1] > 0) {
-      toolToUse = 'iron';
-      picks[1]--;
-    } else if (mineralsCount.stone && picks[2] > 0) {
-      toolToUse = 'stone';
-      picks[2]--;
-    } else {
-      picks.forEach;
-    }
-    console.log(toolToUse);
-    // 위에서 고른 도구로 광물을 캤을 때 피로도 구하기
-    // let curFatigue=0;
-    // for(let mineral in mineralsCount){
-    //     curFatigue+=fatigues[toolToUse][mineral]*mineralsCount[mineral];
-    // }
-    // console.log(curFatigue);
-    // totalFatigue+=curFatigue;
+    fiveMineralsArr.push([fiveMinerals, mineralsCount]);
+  }
+  fiveMineralsArr = fiveMineralsArr
+    .sort((a, b) => {
+      const aDiamond = a[1].diamond || 0;
+      const bDiamond = b[1].diamond || 0;
+      if (aDiamond === bDiamond) {
+        return (b[1].iron || 0) - (a[1].iron || 0);
+      }
+      return bDiamond - aDiamond;
+    })
+    .map(ele => ele[0]);
+  console.log(fiveMineralsArr);
+  let toolIdx = picks[0] ? 0 : picks[1] ? 1 : 2;
+  for (const fiveMinerals of fiveMineralsArr) {
+    totalFatigue += fiveMinerals.reduce((acc, cur) => acc + fatigues[toolIdx][cur], 0);
+    if (--picks[toolIdx] <= 0) toolIdx++;
+    if (picks.every(ele => ele === 0)) return totalFatigue;
   }
   return totalFatigue;
 }
 
 solution([1, 3, 2], ['diamond', 'diamond', 'diamond', 'iron', 'iron', 'diamond', 'iron', 'stone']);
 // 12
-solution(
-  [0, 1, 1],
-  ['diamond', 'diamond', 'diamond', 'diamond', 'diamond', 'iron', 'iron', 'iron', 'iron', 'iron', 'diamond']
-);
+// solution(
+//   [0, 1, 1],
+//   ['diamond', 'diamond', 'diamond', 'diamond', 'diamond', 'iron', 'iron', 'iron', 'iron', 'iron', 'diamond']
+// );
 // 50
