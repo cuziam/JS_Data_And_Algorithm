6baea1b16c539b541dd75a08e2567aca63850bf3
cuziam <yameame320@gmail.com>
2024-11-13 14:42:14 +0900

    베스트-앨범 new solution

--- programmars-review/베스트-앨범.js
+++ programmars-review/베스트-앨범.js
@@ -31,3 +31,44 @@ function solution(genres, plays) {
     });
   return answer;
 }
+
+function solution(genres, plays) {
+  //장르별 정보 삽입
+  let genreInfo = {};
+  genres.forEach((genre, idx) => {
+    if (!genreInfo[genre]) {
+      genreInfo[genre] = {
+        plays: 0,
+        songs: [],
+      };
+    }
+    genreInfo[genre].plays += plays[idx];
+    genreInfo[genre].songs.push([idx, plays[idx]]);
+  });
+
+  console.log(genreInfo);
+
+  //장르별 정보 정렬
+  genreInfo = Object.entries(genreInfo);
+  genreInfo.sort(([, dict1], [, dict2]) => dict2.plays - dict1.plays);
+  console.log(genreInfo);
+
+  genreInfo.forEach(([, dict1]) => {
+    dict1.songs.sort(([num1, plays1], [num2, plays2]) => {
+      if (plays1 > plays2) {
+        return -1;
+      } else if (plays1 === plays2) {
+        if (num1 < num2) return -1;
+      } else {
+        return 1;
+      }
+    });
+  });
+
+  //결과값 추출
+  const result = [];
+  genreInfo.forEach(([, dict]) => {
+    return result.push(...dict.songs.slice(0, 2));
+  });
+  return result.map(song => song[0]);
+}
