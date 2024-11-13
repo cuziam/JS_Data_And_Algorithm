1f7555633468e2906ea82e2fc857891875ec5737
cuziam <yameame320@gmail.com>
2024-11-13 13:16:31 +0900

    완주하지 못한 선수 new solution

--- programmars-review/완주하지-못한-선수.js
+++ programmars-review/완주하지-못한-선수.js
@@ -1,10 +1,10 @@
 function solution(participant, completion) {
-  var answer = '';
-  let counter = {};
+  const answer = '';
+  const counter = {};
   participant.forEach(ele => {
     counter[ele] = (counter[ele] || 0) + 1;
   });
-  for (let finisher of completion) {
+  for (const finisher of completion) {
     if (counter[finisher] >= 2) {
       return finisher;
     }
@@ -15,12 +15,20 @@ function solution(participant, completion) {
   return Object.keys(counter)[0];
 }
 
-function solution(participant, completion) {
-  var answer = '';
+function solution2(participant, completion) {
+  const answer = '';
   participant.sort();
   completion.sort();
 
   for (let i = 0; i < participant.length; i++) {
+    if (participant[i] !== completion[i]) return participant[i];
+  }
+}
+
+function solution3(participant, completion) {
+  participant.sort();
+  completion.sort();
+  for (const i in participant) {
     if (participant[i] !== completion[i]) return participant[i];
   }
 }
